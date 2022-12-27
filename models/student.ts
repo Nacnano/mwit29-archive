d4e28d8055db0f54a1080bbd27b150d26bf632f9
nac <chotpisit080916c@gmail.com>
2022-12-28 01:19:38 +0700

    add and apply prettier

--- models/student.ts
+++ models/student.ts
@@ -1,19 +1,19 @@
 export type Student = {
-    id: string
-    mwit: number
-    room: number
-    firstname: string
-    lastname: string 
-    nickname: string
-    study: string
-    major: string[]
-    fields: string[]
-    subject?: string[]
-    favorites: string[]
-    quote: string
-    tel?: string
-    fb?: string
-    line?: string
-    ig?: string
-    active: boolean | true
-}
+  id: string;
+  mwit: number;
+  room: number;
+  firstname: string;
+  lastname: string;
+  nickname: string;
+  study: string;
+  major: string[];
+  fields: string[];
+  subject?: string[];
+  favorites: string[];
+  quote: string;
+  tel?: string;
+  fb?: string;
+  line?: string;
+  ig?: string;
+  active: boolean | true;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Footer from "../components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -118,6 +119,8 @@ export default function Home() {
           </a>
         </div>
       </main>
+
+      <Footer />
     </>
-  )
+  );
 }
