63f660eb5f758304d9c8a7b755c35bd0393a32ec
nac <chotpisit080916c@gmail.com>
2022-12-28 02:08:01 +0700

    feat: add Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,32 @@
 function Footer() {
   return (
-    <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-      <span className="text-md text-gray-500 sm:text-center dark:text-gray-400">
-        © 2022
-        <a href="https://flowbite.com/" className="hover:underline">
-          Flowbite™
+    <footer className="bg-white rounded-lg shadow px-6 py-5">
+      <div className="flex justify-center">
+        <ul className="flex text-lg text-gray-500">
+          <li>
+            <a href="NOTED" className="mx-4 hover:underline">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="NOTED" className="mx-4 hover:underline">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="NOTED" className="mx-4 hover:underline">
+              Code
+            </a>
+          </li>
+        </ul>
+      </div>
+      <hr className="my-2 w-80 rounded border-gray-300 mx-auto" />
+      <span className="block text-md text-gray-500 text-center">
+        Designed and Developed by{" "}
+        <a href="NOTED" className="hover:underline">
+          Nacnano
         </a>
-        . All Rights Reserved.
       </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6 ">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6">
-            Privacy Policy
-          </a>
-        </li>
-        <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6">
-            Licensing
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:underline">
-            Contact
-          </a>
-        </li>
-      </ul>
     </footer>
   );
 }
