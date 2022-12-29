7e0beb8613fbc0f613dd10c2b8ddd76fdb51204e
nac <chotpisit080916c@gmail.com>
2022-12-29 19:51:33 +0700

    feat: modernize header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,37 +1,64 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Header() {
   return (
-    <nav className="flex bg-white px-2 py-2.5 rounded-b-lg shadow fixed w-full z-20 top-0 left-0 border-b">
-      <div className="container flex items-center justify-between mx-auto text-gray-800">
-        <a href="#" className="flex items-center">
-          {/* NOTED ADD IMAGE*/}
-          <span className="self-center text-xl whitespace-nowrap">MWIT 29 Archive</span>
-        </a>
+    <div className="sticky flex-none top-0 py-2 bg-white rounded-b-lg shadow fixed w-full z-20 left-0 border-b">
+      <div className="max-w-8xl mx-auto text-gray-800 ">
+        <div className="py-2 mx-4">
+          <div className="relative flex items-center">
+            <Link href="/" className="flex items-center w-100">
+              <Image
+                src="/portraits/07967.jpg"
+                className="mr-3 my-1 self-center"
+                width="100"
+                height="100"
+                alt="mwit29-archive-logo"
+              ></Image>
+              <span className="self-center text-xl sm:whitespace-nowrap">
+                MWIT 29 Archive
+              </span>
+            </Link>
 
-        <div className="flex order-2 ">
-            <a href="NOTED" className="text-white text-center bg-blue-700 hover:bg-blue-800 focus:outline-none rounded px-5 py-2.5 mr-3"> Login </a>
-        </div>
-
-        <div className="items-center justify-between w-full">
-          <ul className="flex p-4">
-            <li>
-              <a href="#" className="block mx-4 rounded hover:text-blue-700">
-                Rooms
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block mx-4 rounded hover:text-blue-700">
-                Students
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block mx-4 rounded hover:text-blue-700">
-                Statistics
-              </a>
-            </li>
-          </ul>
+            <div className="relative flex ml-auto items-center w-auto">
+              <nav className="leading-6">
+                <ul className="flex space-x-5">
+                  <li>
+                    <a href="#" className="block rounded hover:text-blue-700">
+                      Rooms
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="block rounded hover:text-blue-700">
+                      Students
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="block rounded hover:text-blue-700">
+                      Gallery
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="block rounded hover:text-blue-700">
+                      Statistics
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="flex border-l border-slate-200 ml-6 pl-6 items-center">
+                <a
+                  href="NOTED"
+                  className="text-white text-center bg-blue-700 hover:bg-blue-800 focus:outline-none rounded-lg px-5 py-2.5 mr-3"
+                >
+                  {" "}
+                  Login{" "}
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
 
     /* <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
