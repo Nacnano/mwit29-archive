b393f15aa5e5ed873637c4fb8f29845af852de79
nac <chotpisit080916c@gmail.com>
2022-12-31 00:28:29 +0700

    feat: add Quote and prepare for RoomCards

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,33 @@ export default function Home() {
       </Head>
 
       <Header />
-      <main className="mb-auto max-w-4xl flex w-full flex-1 h-screen flex-col items-center justify-center px-10 text-center overflow-hidden">
-        <div className="items-center">
-          <a> Coming Soon...</a>
+      <main className="mb-auto max-w-5xl flex w-full flex-1 h-screen flex-col items-center justify-center px-10 text-center overflow-hidden">
+        <div className="">
+          <div className="flex flex-col items-center text-3xl font-medium text-center mr-10">
+            <div>&quot;I dreamed to be a doctor for my whole life.</div>
+            <div>
+              But being at MWIT, it made me realized that I could be a
+              doctor.&quot;
+            </div>
+          </div>
+          <div className="mt-5 place-self-end text-2xl font-normal text-right pb-20">
+            Nac, Computer Engineer
+          </div>
         </div>
       </main>
+
+      <section className="relative -mt-20 w-full max-w-4xl flex flex-col items-start justify-start">
+        <div className="w-full flex flex-col items-center">
+          <h1 className="text-4xl font-bold">Rooms</h1>
+        </div>
+        <div className="w-full flex flex-col justify-center">
+          <div className="flex flex-row flex-wrap justify-center items-center gap-5">
+            {
+              //  Add RoomCards here
+            }
+          </div>
+        </div>
+      </section>
       <Footer />
     </div>
   );
