c82dab449e8ed0101592d151819cff2c5d643d05
nac <chotpisit080916c@gmail.com>
2022-12-31 21:44:53 +0700

    feat: change Home page style

--- components/RoomCard.tsx
+++ components/RoomCard.tsx
@@ -14,7 +14,7 @@ const RoomCard = ({ room }: Props) => {
         query: { id: room.id },
       }}
       as={`/room/${room.id}`}
-      className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 hover:scale-110"
+      className="max-w-sm bg-gray-100 border border-gray-200 rounded-lg shadow-md hover:scale-110"
     >
       <div>
         <img
@@ -25,29 +25,30 @@ const RoomCard = ({ room }: Props) => {
           // objectFit="contain"
         />
       </div>
-      <div className="p-5">
+      <div className="flex flex-col p-5">
         <div className="items-center justify-start gap-2">
           <h1 className="text-1xl font-normal tracking-tight text-gray-900">Room {room.id}</h1>
 
           <h2 className="text-2xl font-semibold align-middle text-gray-700">{room.name}</h2>
         </div>
-
-        {/* <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          {room.description}
-          <svg
-            aria-hidden="true"
-            className="w-4 h-4 ml-2 -mr-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </div> */}
+        <div className="flex justify-end">
+          <div className="inline-flex items-center px-3 py-2 text-medium font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+            {room.quote}
+            <svg
+              aria-hidden="true"
+              className="w-4 h-4 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </div>
       </div>
     </Link>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,27 +23,27 @@ export default function Home({ sortedRooms }: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="bg-gray-100 w-screen h-screen flex flex-col items-center">
+      <div className="bg-gray-100 w-screen h-full flex flex-col items-center">
         <Header />
         <div>
-          <div className="relative max-w-5xl flex w-full flex-1 h-screen flex-col items-center justify-center px-10 text-center overflow-hidden">
+          <div className="bg-gray-200 relative max-w-8xl flex w-screen flex-1 h-screen flex-col items-center justify-center px-10 mb-10 text-center">
             <div className="">
-              <div className="flex flex-col items-center text-3xl font-medium text-center mr-10">
+              <div className="flex flex-col items-center text-4xl font-normal text-center mr-10 gap-3">
                 <div>&quot;I dreamed to be a doctor for 16 years.</div>
                 <div>But being at MWIT, it made me realized that I could be a doctor.&quot;</div>
               </div>
-              <div className="mt-5 place-self-end text-2xl font-normal text-right pb-20">
+              <div className="mt-5 text-2xl font-normal text-right pb-20">
                 Nac, Computer Engineer
               </div>
             </div>
           </div>
 
-          <div className="relative mt-5 px-10 w-full max-w-5xl flex flex-col items-center justify-center">
+          <div className="relative mt-0 px-10 mb-10 w-full max-w-8xl flex flex-col items-center justify-center">
             <div className="w-full flex flex-col items-center">
               <h1 className="text-4xl font-bold">Rooms</h1>
             </div>
             <div className="mt-5 w-full flex flex-col justify-center">
-              <div className="flex flex-row flex-wrap justify-center items-center gap-10">
+              <div className="flex flex-row flex-wrap justify-center items-center gap-5">
                 {sortedRooms.map((room) => {
                   return <RoomCard key={room.id} room={room} />
                 })}
