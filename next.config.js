01e253e063cf811db0f153dcf4b9c90643b90627
nac <chotpisit080916c@gmail.com>
2022-12-31 20:47:13 +0700

    feat: add vscode prettier format

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    appName: "MWIT29 Archive",
-    // appUrl: 'https://mwit29-archive.vercel.app',
-    appUrl: "http://localhost:3000",
-  },
   reactStrictMode: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
