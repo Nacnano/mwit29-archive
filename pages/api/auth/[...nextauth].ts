d694c37b9091a3643085874cff9d0a7a1005a064
nac <chotpisit080916c@gmail.com>
2023-01-22 23:09:56 +0700

    feat: add restriction for specific email domain

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import { Account, Profile } from 'next-auth'
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
 
@@ -6,15 +7,25 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+
+      // callbacks: {
+      //   async signIn({ account, profile }) {
+      //     if (account.provider === 'google') {
+      //       return profile.email_verified && profile.email.endsWith('@example.com')
+      //     }
+      //     return true // Do different verification for other providers that don't have `email_verified`
+      //   },
+      // },
     }),
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === 'google') {
+        // Type warnings here I will leave it like this... Fix soon?
+        return profile.email_verified && (profile.email as string).endsWith('@mwit.ac.th')
+      }
+      return true
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
-  // callbacks: {
-  //   async signIn({ account, profile }) {
-  //     if (account.provider === 'google') {
-  //       return profile.email_verified && profile.email.endsWith('@example.com')
-  //     }
-  //     return true // Do different verification for other providers that don't have `email_verified`
-  //   },
-  // },
 })
